165bafa3bc35bcc87b14d6d3b9b795a5321362f4
Alex <zetterstrom.alexander@gmail.com>
2021-09-27 19:42:28 +0200

    swapped for updates files

--- U1/index.js
+++ U1/index.js
@@ -1,21 +1,8 @@
 "use strict";
-/*
 
-Så följande:
-
-async function getData(){
-const promise = await fetch(URL);
-const data = await promise.json();
-return data;
-}
-
-async function doingSth(){
-const data = await getData();
-}
-
-*/
-
-//Grab all users, to be used every 30 sec with interval
+// ***********************************************************
+//*************************VARIABLES**************************
+//************************************************************
 
 const userURL = `http://mpp.erikpineiro.se/dbp/sameTaste/users.php`;
 
@@ -30,6 +17,10 @@ const mainUser = 21;
 const testerUser = 666;
 let selectedUser = mainUser;
 
+// ***********************************************************
+//*************************GRAB USERS**************************
+//************************************************************
+
 async function grabUsers() {
   const response = await fetch(userURL);
   const data = await response.json();
@@ -37,26 +28,17 @@ async function grabUsers() {
   return data.message;
 }
 
-if (localStorage.length <= 0) {
-  ("hej");
-  loadingBarPaintingsWindow();
-}
-
 setInterval(displayUsers, 30000);
 displayUsers();
 
-function userListTimer() {
-  let i = 29;
-  let timer = document.createElement("div");
-  timer.classList.add("timer");
-  timer.innerHTML = `Time until refresh: ${i}`;
-  setInterval(() => {
-    i--;
-    timer.innerHTML = `Time until refresh: ${i}`;
-  }, 3000);
+// ***********************************************************
+//*************************LOADING SCREENS*********************
+//************************************************************
 
-  document.querySelector("#listOfUsers").append(timer);
-  // setTimeout(() => {clearInterval(intervalID)}, 30000)
+//If local storage is empty then it will display a loading screen for the pictures while they are fetched from the API.
+if (localStorage.length <= 0) {
+  ("hej");
+  loadingBarPaintingsWindow();
 }
 
 function loadingBarUserList() {
@@ -95,6 +77,35 @@ function loadingBarPaintingsWindow() {
   }, 5000);
 }
 
+// ***********************************************************
+//*************************TIMER********************
+//** *********************************************************
+
+//Creates a timer DOM with an interval that updates the dom every second, which after 30 seconds gets cleared.
+function userListTimer() {
+  let i = 29;
+  let timer = document.createElement("div");
+  timer.classList.add("timer");
+  timer.innerHTML = `Time until refresh: ${i}`;
+  let timerInterval = setInterval(() => {
+    i--;
+    timer.innerHTML = `Time until refresh: ${i}`;
+  }, 1000);
+
+  document.querySelector("#listOfUsers").append(timer);
+  setTimeout(() => {
+    clearInterval(timerInterval);
+  }, 30000);
+}
+
+// ***********************************************************
+//*************************USER PRINTS********************
+//** *********************************************************
+
+/*Gets the promise from grabUsers which contains an array of all the users, which then gets saved into a resolved array of users.
+Separates main and tester users from the array with a filter after finding both objects and saving them in a variable. After which it takes all the info from the arrays and calls on crateUsers arrays with arguments from the variables or arrays containing user info. After which it calls on displayPictures to display paintings on load of website. 
+
+*/
 async function displayUsers() {
   document.querySelector("#listOfUsers").innerHTML = "";
   let arrayOfUsers = await grabUsers();
@@ -150,6 +161,8 @@ async function displayUsers() {
 
   displayPictures();
 }
+
+//Function that crates the main users, with parameters of the name of the user, amount of favorites and the users ID. If the selected user is the mainuser (Which it is by default) then it'll add the dom as selected so its the selected one for first time loading of the website. Then it adds the main user class on the DOM and an event listener which will make the selected user the ID it was provided in the function. If there is a previous selected DOM then it'll find it and remove the selected class from it and add the class to the new one. After it'll call on displayPictures() so the pictures gets displayed after a user is selected. The functions for testers and users follow the same structure.
 
 function createMainUser(alias, amountOfFavs, id) {
   let nameTag = document.createElement("div");
@@ -222,12 +235,22 @@ function createUserDivs(alias, amountOfFavs, commonFavs, id) {
   document.querySelector("#listOfUsers").append(nameTag);
 }
 
+// ***********************************************************
+//*************************GRAB PICTURES********************
+//** *********************************************************
+
 async function grabPictureIDs() {
   const response = await fetch(pictureIDURL);
   const data = await response.json();
 
   return data.objectIDs;
 }
+
+// ***********************************************************
+//*************************DISPLAY PICTURES*******************
+//** *********************************************************
+
+//Gets the promise of an array with painting IDS which is then saved into a variable after resolve. Then these IDs are put into a new array of promises which we call on grabPicture array with element as arguments. Afterwards it goes through a promise.all and after we go through each element and save everything in localStorage. We also save photo information in a global array.
 
 async function savePhotos() {
   let arrayOfPictureIDs = await grabPictureIDs();
@@ -253,6 +276,8 @@ async function savePhotos() {
   displayPictures();
 }
 
+//If an id is present in local storage then it returns an object with the paint information otherwise we fetch it from the API. Picture ID so it knows to search for either the title of the local storage or api URL.
+
 async function grabPicture(pictureID) {
   if (localStorage.getItem(`${pictureID}.Title`)) {
     return {
@@ -269,14 +294,9 @@ async function grabPicture(pictureID) {
   return data;
 }
 
-function findMainUser() {
-  let foundMainUser = userArray.find((user) => {
-    return user.alias == "Alexander";
-  });
-  return foundMainUser.id;
-}
-
 savePhotos();
+
+// If the selecteduser is not the main user then it finds a user within the userarray if it matches with selecteduser then it finds the object and the array of favorites inside. The elements of the array is then used to call on grabpicture array and then displays those favorite photos.  if the main user is selecteduser id then it displays all pictures instead.
 
 async function displayPictures() {
   document.querySelector("#paintingWindow").innerHTML = "";
@@ -297,6 +317,8 @@ async function displayPictures() {
     SortedarrayOfImages.forEach((images) => createPictureDOM(images));
   }
 }
+
+//Creates the DOMs to be displayed in a window of paintings. TWo buttons with event listeners are included which calls on either a remove favorite function or add favorite function.
 
 function createPictureDOM(picture) {
   let pictureDivWrapper = document.createElement("div");
@@ -346,7 +368,7 @@ function createPictureDOM(picture) {
   document.querySelector("#paintingWindow").append(pictureDivWrapper);
 }
 
-//Adds a favorite painting by calling the api
+//Adds a favorite painting by calling the api and provides an object of information including the method and content type necessary.
 async function addPictureToFavs(pictureFavID, divWrapper) {
   let URL = await fetch(
     new Request(userURL, {
@@ -357,12 +379,13 @@ async function addPictureToFavs(pictureFavID, divWrapper) {
       body: JSON.stringify({ id: mainUser, addFav: pictureFavID }),
     })
   );
-  errorHandler(URL.status, "add", divWrapper);
+  responseHandler(URL.status, "add", divWrapper);
   displayUsers();
   console.log("Added Picture", pictureFavID);
 }
 
-//Removes a favorite painting by calling the api
+//Removes a favorite painting by calling the api and provides an object of information including the method and content type necessary. Afterwards the responseHandler gets called which will display the necessary feedback after an action.
+
 async function removePictureToFavs(pictureFavID) {
   let URL = await fetch(
     new Request(userURL, {
@@ -374,13 +397,13 @@ async function removePictureToFavs(pictureFavID) {
     })
   );
 
-  errorHandler(URL.status, "remove");
+  responseHandler(URL.status, "remove");
   displayUsers();
   console.log("Remove Picture", pictureFavID);
 }
 
 //Handles all promise errors and displays an alert window
-function errorHandler(fetch, addOrRemove, divWrapper) {
+function responseHandler(fetch, addOrRemove, divWrapper) {
   if (fetch == 409) {
     divWrapper.classList.remove("favoritePictures");
     window.alert("You have added too many favorites");
